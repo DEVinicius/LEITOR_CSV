5b25036fa4c1d480eee7e072119f01e7808bc657
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2020-02-04 21:41:21 -0300

    Adição dos primeiros arquivos e configuração da classe CSV

---
+++ leitor.py
@@ -0,0 +1,26 @@
+import csv
+
+class CSV:
+        
+    def ler_csv(arquivo_csv):    
+        with open(arquivo_csv,"r") as csvfile:
+            spamreader = csv.reader(csvfile, delimiter = ",")
+            line_count = 0
+            for row in spamreader:
+                if line_count == 0:
+                    print(f'{"  ".join(row)}')
+                    line_count += 1
+                else:
+                    for linha in row:
+                        print(linha)
+                    line_count += 1
+                print(f"linhas processadas {line_count}")
+    
+    def escrever_csv(arquivo_csv):
+        with open(arquivo_csv, "w") as csvfile:
+            csvwrite = csv.writer(csvfile, ",")
+
+        codigo = input("Código do Produto")
+        nome = input("Nome do Produto")
+
+
